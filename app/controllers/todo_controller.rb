a077c55086fe6415286149515db61b458b1b1430
alexis <santosalexis632@yahoo.com>
2018-11-17 16:51:56 +0000

    description controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,12 @@
 class TodoController < ApplicationController
     def index
     end
+    def show 
+    end
+    def show 
+    @todo_description =
+    @todo_pomopdoro_estimate = b 
 end
+    
+end 
+    
