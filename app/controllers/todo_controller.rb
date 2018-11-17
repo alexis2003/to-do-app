d45dd6a0f0ba049627a6400408dfd99edc5ae1af
alexis <santosalexis632@yahoo.com>
2018-11-17 20:11:00 +0000

    update

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,11 +2,14 @@ class TodoController < ApplicationController
     def index
     end
     def show 
+       b = 2
+        todo_id="1"  
+        if todo_id=="1"
+            @todo_description="s"
+            @todo_pomopdoro_estimate = b
+        elsif  todo_id=="2"
+            @todo_description="s"
+            @todo_pomopdoro_estimate = b
+        end
     end
-    def show 
-    @todo_description =
-    @todo_pomopdoro_estimate = b 
-end
-    
 end 
-    
